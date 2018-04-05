b51dce72da0729c8712cd5150bebc5aefbdae259
oliviaposner <oposner@fwparker.org>
2018-04-05 13:49:42 -0500

    Merge branch 'master' of https://github.com/fwparkercode/Programming2
    # Conflicts:
    #	test.py

--- energy_scatter_plot.py
+++ energy_scatter_plot.py
@@ -24,3 +24,70 @@ Challenge (for fun):
 - Add colleges and universities (use a different marker type)
 
 '''
+
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+with open("search_files/Chicago_Energy_Benchmarking_-_2016_Data_Reported_in_2017.csv") as f:
+    reader = csv.reader(f)
+    data = list(reader)
+
+print(data)
+schools_to_plot = []
+
+new_data = []
+for i in range(len(data)):
+    try:
+        ghg_intensity = float(data[i][21])
+        new_data.append(data[i])
+    except:
+        pass
+
+data = new_data[:]
+
+for i in range(len(data)):
+    if data[i][6] == "K-12 School" and data[i][21] != " ":
+            schools_to_plot.append(data[i])
+
+schools_to_plot.sort(key=lambda x: float(x[21]))
+
+print(schools_to_plot)
+print(len(schools_to_plot))
+
+ghg_emissions = []
+square_footage = []
+school_names = []
+
+for i in range(len(schools_to_plot)):
+        try:
+            emissions = float(schools_to_plot[i][20])
+            ghg_emissions.append(emissions)
+            footage = float(schools_to_plot[i][7])
+            square_footage.append(footage)
+            names = schools_to_plot[i][2]
+            if names == "Francis W Parker School" or i < 5 or i > (len(schools_to_plot) - 5):
+                school_names.append(names)
+            else:
+                school_names.append("")
+        except:
+             print(data[i][0], "has no data.")
+
+plt.figure(1, figsize=(12, 7), facecolor="green")
+
+plt.scatter(square_footage, ghg_emissions, s=4, c="yellow", marker='+')
+plt.title("Energy Efficiency of Chicago Schools", color='yellow', fontsize=25)
+plt.xlabel("School Square Footage", color='yellow')
+plt.ylabel("Green House Gas Emissions", color='yellow')
+
+m, b = np.polyfit(square_footage, ghg_emissions, 1) # 1 for linear, returns slope and y intercept
+
+x = [0, 700000]
+y = [m * point + b for point in x]
+plt.plot(x, y, color='orange')
+
+for i in range(len(school_names)):
+    plt.annotate(school_names[i], xy=(square_footage[i], ghg_emissions[i]), fontsize=8) # text, xy =()
+
+plt.show()
--- plotting_data_problems.py
+++ plotting_data_problems.py
@@ -1,3 +1,7 @@
+import csv
+import matplotlib.pyplot as plt
+
+
 
 # MATPLOTLIB PROBLEM # 1
 # Chicago Public Library Visitors (15pts)
@@ -8,6 +12,83 @@
 # label the x with the month.  Rotate the text so we can read it.  (see example problem).  Use the tight_fit command to show all text.
 # label axes, title the graph as necessary.
 
+with open("files/Libraries_-_2017_Visitors_by_Location.csv") as f:
+    reader = csv.reader(f)
+    data = list(reader)
+
+print(data)
+
+month_numbers = [x for x in range(12)]
+print(month_numbers)
+
+month_names = data[0][1:-1]
+print(month_names)
+
+
+
+
+jan = [x[1] for x in data][1:]
+jan = [int(x) for x in jan]
+print(sum(jan))
+
+feb = [x[2] for x in data][1:]
+feb = [int(x) for x in feb]
+print(sum(feb))
+
+mar = [x[3] for x in data][1:]
+mar = [int(x) for x in mar]
+print(sum(mar))
+
+apr = [x[4] for x in data][1:]
+apr = [int(x) for x in apr]
+print(sum(apr))
+
+may = [x[5] for x in data][1:]
+may = [int(x) for x in may]
+print(sum(may))
+
+jun = [x[6] for x in data][1:]
+jun = [int(x) for x in jun]
+print(sum(jun))
+
+jul = [x[7] for x in data][1:]
+jul = [int(x) for x in jul]
+print(sum(jul))
+
+aug = [x[8] for x in data][1:]
+aug = [int(x) for x in aug]
+print(sum(aug))
+
+sept = [x[9] for x in data][1:]
+sept = [int(x) for x in sept]
+print(sum(sept))
+
+oct = [x[10] for x in data][1:]
+oct = [int(x) for x in oct]
+print(sum(oct))
+
+nov = [x[11] for x in data][1:]
+nov = [int(x) for x in nov]
+print(sum(nov))
+
+dec = [x[12] for x in data][1:]
+dec = [int(x) for x in dec]
+print(sum(dec))
+
+visitors = [sum(jan), sum(feb), sum(mar), sum(apr), sum(may), sum(jun), sum(jul), sum(aug), sum(sept), sum(oct), sum(nov), sum(dec)]
+
+plt.bar(month_numbers, visitors)
+
+plt.xticks(month_numbers, month_names, rotation=45, color="red")
+
+plt.figure(1, figsize=(-10, 20), tight_layout=True, facecolor="yellow")
+# plt.bar(month_numbers, month_names, color='red')
+plt.title("Usage of Chicago Libraries Per Month - 2017", color="red")
+plt.ylabel("Computer Users", color="red")
+
+
+
+
 # MATPLOTLIB PROBLEM # 2 
 # Chicago Public Transit Usership Graph (20pts)
 # go to https://data.cityofchicago.org/Transportation/CTA-Ridership-Annual-Boarding-Totals/w8km-9pzd
@@ -17,3 +98,46 @@
 # Make each line, points, and color different for the four graphs.
 # Make a legend to identify each line.
 # Label axes and give your graph a title.  Change it in any other way you see necessary to give it a clean look.
+
+with open("search_files/CTA_-_Ridership_-_Annual_Boarding_Totals.csv") as f:
+    reader = csv.reader(f)
+    data = list(reader)
+print(data)
+
+
+
+year_names = [x[0] for x in data][1:]
+print(year_names)
+
+year_numbers = [x for x in range(len(year_names))]
+print(year_numbers)
+
+
+
+bus = [x[1] for x in data][1:]
+bus = [int(x) for x in bus]
+
+paratransit = [x[2] for x in data][1:]
+paratransit = [int(x) for x in paratransit]
+
+rail = [x[3] for x in data][1:]
+rail = [int(x) for x in rail]
+
+total = [x[4] for x in data][1:]
+total = [int(x) for x in total]
+
+plt.figure(2, tight_layout=True)
+
+plt.plot(year_numbers, bus, label="Bus")
+plt.plot(year_numbers, paratransit, label="Paratransit")
+plt.plot(year_numbers, rail, label="Rail")
+plt.plot(year_numbers, total, label="Total")
+plt.xticks(year_numbers, year_names, rotation=45)
+
+plt.legend(bbox_to_anchor=(0, 1), loc="upper left")
+plt.xlabel("Years")
+plt.ylabel("Transit Boarding Totals")
+plt.title("CTA Ridership Annual Boarding Totals")
+
+
+plt.show()
